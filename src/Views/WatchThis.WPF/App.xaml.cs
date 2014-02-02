ab5eb1087a0235c8cb9d7a1566896ac598581b05
kevintavog <kevintavog@zoho.com>
2014-02-02 11:49:09 -0800

    WPF handles files at startup (programmatic file association appears to require admin privilege. Blech).
    Fix the NLog file; the name included the date...

--- src/Views/WatchThis.WPF/App.xaml.cs
+++ src/Views/WatchThis.WPF/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.IO;
 using System.Windows;
 using WatchThis.Models;
@@ -10,6 +11,38 @@ namespace WatchThis.Wpf
     /// </summary>
     public partial class App : Application
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        private void ApplicationStart(object sender, StartupEventArgs e)
+        {
+            Window main = null;
+            var args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                logger.Info("Command line argument: {0}", args[1]);
+                try
+                {
+                    var model = SlideshowModel.ParseFile(args[1]);
+                    main = new SlideshowWindow(model);
+                }
+                catch (Exception ex)
+                {
+                    logger.Error("Failed loading '{0}': {1}", args[1], ex);
+                    MessageBox.Show(
+                        string.Format("Unable to open {0}: {1}", args[1], ex.Message),
+                        "Error loading file",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                }
+            }
+
+            if (main == null)
+            {
+                main = new SlideshowListView();
+            }
+            main.Show();
+        }
+
         public App()
         {
             Preferences.Load(Path.Combine(
